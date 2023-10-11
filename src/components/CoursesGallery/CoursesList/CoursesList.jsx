0f1bdb6d26069a1330e58293ff82709b082b70c9
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-10-11 17:35:17 +0300

    ADD: courses list

--- src/components/CoursesGallery/CoursesList/CoursesList.jsx
+++ src/components/CoursesGallery/CoursesList/CoursesList.jsx
@@ -1,10 +1,45 @@
 import { CourseCard } from '../CourseCard';
+import courses from '../../../courses.json';
+import { List } from './CoursesList.styled';
+
 
 const CoursesList = () => {
+    console.log(courses);
     return (
-        <ul>
-            <CourseCard/>
-        </ul>
+        <List>
+            {
+                courses.map(({
+                    id,
+                    title,
+                    author,
+                    experience,
+                    duration,
+                    price,
+                    lessonFormat,
+                    category,
+                    subcategory,
+                    lessonLenguage,
+                    content,
+                    avatar
+                }) =>
+                <CourseCard
+                    key={id}
+                    title={title}
+                    author={author}
+                    experience={experience}
+                    duration={duration}
+                    price={price}
+                    lessonFormat={lessonFormat}
+                    category={category}
+                    subcategory={subcategory}
+                    lessonLenguage={lessonLenguage}
+                    content={content}
+                    avatar={avatar}                
+                />
+            )
+            }
+            
+        </List>
     );
 };
 
