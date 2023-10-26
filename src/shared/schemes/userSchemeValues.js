39f2999b3a228607635f4fe62ac46ceae5c724b9
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-10-26 12:33:27 +0300

    FIX: quotes

--- src/shared/schemes/userSchemeValues.js
+++ src/shared/schemes/userSchemeValues.js
@@ -10,7 +10,7 @@ export const nameScheme = Yup
     .string()
     .min(FORMS_VALIDATION.minName, 'Name must have at least 2 characters')
     .max(FORMS_VALIDATION.maxName, 'Name can not have more then 16 characters')
-    .required("Name is required");
+    .required('Name is required');
 
 
 export const emailScheme = Yup
@@ -19,33 +19,33 @@ export const emailScheme = Yup
         EMAIL_REGEXP,
         'Invalid email format'
     )
-    .required("Email is required");
+    .required('Email is required');
 
 
 export const passwordScheme = Yup
     .string()
     .matches(
         PASSWORD_REGEXP,
-        "Password must contain at least 6 characters, one uppercase, one number and one special case character"
+        'Password must contain at least 6 characters, one uppercase, one number and one special case character'
     )
-    .required("Password is required");
+    .required('Password is required');
 
 
 export const currentPasswordScheme = Yup
     .string()
     .matches(
         PASSWORD_REGEXP,
-        "Password must contain at least 6 characters, one uppercase, one number and one special case character"
+        'Password must contain at least 6 characters, one uppercase, one number and one special case character'
     )
     .required('Enter current password is required');
 
 
 export const confirmPasswordScheme = Yup
     .string()
-    .required("Conformation is required")
-    .oneOf([Yup.ref("password"), null], "Passwords must match");
+    .required('Conformation is required')
+    .oneOf([Yup.ref('password'), null], 'Passwords must match');
     
     
 export const confirmationScheme = Yup
     .bool()
-    .oneOf([true], "You must accept the terms and conditions");
+    .oneOf([true], 'You must accept the terms and conditions');
