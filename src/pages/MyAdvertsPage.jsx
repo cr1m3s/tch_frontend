672bea130b1699897e4db84e4d0d7d599628014e
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-11-06 10:43:23 +0200

    FIX: remove test part of code

--- src/pages/MyAdvertsPage.jsx
+++ src/pages/MyAdvertsPage.jsx
@@ -1,22 +1,6 @@
-import { Link } from "react-router-dom";
-
-
 const MyAdvertsPage = () => {
     return (
-        <div>
-            <div style={{ marginTop: '50px', marginBottom: '50px', fontSize: '50px', textAlign: 'center' }}>
-                Here will be a list of my posts!
-            </div>
-            
-            <Link
-                to='/add-advert'
-                style={{ fontSize: '30px', backgroundColor: '#D7DD73', padding: '8px', color: '#FFFFFF', borderRadius: '10px' }}
-            >
-                Add advertesment
-            </Link>
-        </div>
-
-
+        <div></div>
     );
 };
 
