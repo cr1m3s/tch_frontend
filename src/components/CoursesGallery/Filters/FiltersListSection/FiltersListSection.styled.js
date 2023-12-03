31c3071d5839d2562af63948240f7d2328541dd4
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-12-03 15:13:01 +0200

    ADD: price-range-slider in filters

--- src/components/CoursesGallery/Filters/FiltersListSection/FiltersListSection.styled.js
+++ src/components/CoursesGallery/Filters/FiltersListSection/FiltersListSection.styled.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { Icon } from '../../../common';
+import RangeSlider from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
 
 
 export const ItemWrapper = styled.div`
@@ -12,6 +14,7 @@ export const ItemWrapper = styled.div`
     }
 `;
 
+
 export const SectionHeader = styled.div`
     display: flex;
     justify-content: space-between;
@@ -20,11 +23,13 @@ export const SectionHeader = styled.div`
     cursor: pointer;
 `;
 
+
 export const Title = styled.div`
     font-weight: 600;
     font-size: 24px;
     line-height: 28px;
 `;
+
 
 export const StyledIcon = styled(Icon)`
     fill: ${({ theme: { colors } }) => colors.white};
@@ -36,6 +41,7 @@ export const StyledIcon = styled(Icon)`
     }
 `;
 
+
 export const CategoriesList = styled.div`
     font-weight: 400;
     font-size: 20px;
@@ -43,12 +49,21 @@ export const CategoriesList = styled.div`
     margin-bottom: 30px;
 `;
 
+
+export const RangeFieldContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`;
+
+
 export const PriceInputsContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 8px;
 `;
+
 
 export const PriceInput = styled.input`
     font-weight: 500;
@@ -59,12 +74,43 @@ export const PriceInput = styled.input`
     border-radius: 6px;
     width: 90px;
     height: 36px;
-    background-color: ${({ theme: { colors } }) => colors.disabled};
+    background-color: #EAEAEA;
     padding-left: 16px;
 `;
+
 
 export const Line = styled.div`
     width: 10px;
     height: 1px;
     background-color: ${({ theme: { colors } }) => colors.disabled};
+`;
+
+
+export const StyledRangeSlider = styled(RangeSlider)`
+    background-color: ${({ theme: { colors } }) => colors.golden};
+    height: 3px;
+
+    .range-slider__range {
+        background-color: transparent;
+    }
+
+    .range-slider__thumb {
+        background-color: ${({ theme: { colors } }) => colors.golden};
+        transition-property: background-color, border, border-radius, width;
+        transition-duration: 250ms;
+        transition-timing-function: cubic-bezier(0.7, 0.7, 1, 0.7);
+    }
+
+    .range-slider__thumb[data-active] {
+        border: 2px solid  ${({ theme: { colors } }) => colors.lightBlue};
+        border-radius: 6px;
+        width: 6px;
+    }
+
+    @media screen and (min-width: ${({ theme: { sizes } }) => sizes.desk}) {
+        .range-slider__thumb {
+            background-color: ${({ theme: { colors } }) => colors.blue};
+        }
+        
+    }   
 `;
